11660977a57d9cbc91a548e18f7098fa1c663cfa
Abu Saeed <abusaeedstst@gmail.com>
2022-11-16 02:36:35 +0600

    Create 7.Bulding a Basic Calclator.py

---
+++ 7.Bulding a Basic Calclator.py
@@ -0,0 +1,22 @@
+## Add those number(here num1,num2 is charrecter number)
+
+#num1 = input("Enter a number: ")
+#num2 = input("Enter another number: ")
+#result =num1+num2
+
+#print(result)
+
+##Addition two string numberr
+#num1 = input("Enter a number: ")
+#num2 = input("Enter another number: ")
+#result =int(num1)+int(num2)
+
+#print(result)
+
+###Addition Decimal (float) nummber
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result =float(num1)+float(num2)
+
+print(result)
