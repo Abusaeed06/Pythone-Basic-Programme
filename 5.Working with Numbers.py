6cd259dab768b29be710275854825ed1aff2e322
Abu Saeed <abusaeedstst@gmail.com>
2022-11-17 15:05:08 +0600

    Create 5.Working with Numbers.py

---
+++ 5.Working with Numbers.py
@@ -0,0 +1,36 @@
+print(2)
+print(2.624827)
+print(-2.4784)
+print(12+24)
+print(12-3)
+print(12*3)
+print(13*3.342)
+print(12/3)
+print(3*4+5)
+print(3*(4+5))
+print(10 % 3)
+My_num=5
+print(My_num)
+print(str(My_num)+" my favorite number")
+#print((My_num)+" my favorite number")  #dosent work
+#dosent work
+h=-5
+print(h)
+print(abs(h))
+# Power function
+print(pow(2,3))
+print(pow(4,6))
+
+#max,min function
+print(max(4,6))
+print(min(4,6))
+
+#round function
+print(round(3.7))
+print(round(3.4))
+
+from math import *
+
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
