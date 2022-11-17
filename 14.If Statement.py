69996ed9c3dce57b67c02a0a80eadd33dd0ae86d
Abu Saeed <abusaeedstst@gmail.com>
2022-11-17 15:06:01 +0600

    Create 14.If Statement.py

---
+++ 14.If Statement.py
@@ -0,0 +1,14 @@
+male=False
+tall=False
+if male or tall:
+    print(" you are male or tall or both")
+else:
+    print("you neither male nor tall ")
+
+ male = True
+ tall = True
+if male and tall:
+     print(" you are a tall male")
+else:
+     print("you are neither male or not tall or both")
+
