725b6b72db36a1d497856e60bfdc94268eedd0de
Abu Saeed <abusaeedstst@gmail.com>
2022-11-18 23:39:55 +0600

    Update 2.variable and data tpe.py

--- 2.variable and data tpe.py
+++ 2.variable and data tpe.py
@@ -15,7 +15,8 @@ age=22
 city="Kushtia"
 cgpa=4.92
 
-print("Our new Student Name is"+name)
+print("Our new Student Name is "+name)
 print(name+" Lives in "+city)
 print("He is currently" ,age,"years old")
 print( "At the" ,age,"years old ,he was started learn pythone")
+
