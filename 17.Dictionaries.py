348f9f2a7646ab7437a61e604706acd39334c63f
Abu Saeed <abusaeedstst@gmail.com>
2022-11-23 00:47:31 +0600

    Create 17.Dictionaries.py

---
+++ 17.Dictionaries.py
@@ -0,0 +1,40 @@
+monthConversion ={
+    "Jan":"January",
+    "Feb": "February",
+    "Mar":"March",
+    "Apr":"April",
+    "May":"May",
+    "Jun":"June",
+    "Jul":"July",
+    "Aug":"August",
+    "Sep":"September",
+    "Oct":"October",
+    "Nov":"November",
+    "Dec":"December",
+}
+
+print(monthConversion["Nov"])
+print(monthConversion["Jan"])
+
+print(monthConversion.get("Dec"))
+print(monthConversion.get("Luv","Not a Valid key"))
+
+# It can also be work properly
+
+monthConversion ={
+    1:"January",
+    2: "February",
+    3:"March",
+    4:"April",
+    5:"May",
+    6:"June",
+    7:"July",
+    8:"August",
+    9:"September",
+    10:"October",
+    11:"November",
+    12:"December",
+
+}
+
+print(monthConversion[12])
