e96b20efe16ae8c2e7aa5f27ac194f31a04678d1
Abu Saeed <abusaeedstst@gmail.com>
2022-11-24 23:13:20 +0600

    Create 20.For Loop.py

---
+++ 20.For Loop.py
@@ -0,0 +1,20 @@
+for letter in " Focasting Funnction":
+    print(letter)
+####
+friends =["JIn","Kim","nadin",]
+for name in friends:
+    print(name)
+
+####
+for index in range (3,10):
+    print(index)
+###
+riends =["JIn","Kim","nadin",]
+for index in range(len(friends)):
+    print(friends[index])
+###
+for index in range(10):
+    if index==0:
+        print("First Itr=eration ")
+    else:
+        print("Not First")
